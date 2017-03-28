d39d08f705205e1e8f86f4d6ecec24e9f78f3655
Simon <homer12345@gmail.com>
2017-03-27 22:45:46 -0700

    Refactored test plugin to match interface update

--- test_plugins/win_simple_perf/win_simple_perf.cpp
+++ test_plugins/win_simple_perf/win_simple_perf.cpp
@@ -1,15 +1,31 @@
 #include <Windows.h>
 
 #include <cstdio>
+#include <functional>
+#include <map>
 #include <plugin_api.h>
 
-static log_func_type logFunc;
+static log_func_type logFunc = nullptr;
 
 const char* pluginName = "Simple Performance Query";
 const char* pluginCode = "win_simple_perf";
 
-const char *cpuSource = "cpu";
-const char *ramSource = "ram";
+using GetDataFnType = std::function<int(char*, int, int*)>;
+using DataCallTable = std::map<unsigned int, GetDataFnType>;
+
+static DataCallTable calltable;
+
+enum PerfDataSources
+{
+    PERF_SRC_CPU = 0,
+    PERF_SRC_RAM
+};
+
+QuasarPluginDataSource sources[2] =
+{
+    { "cpu", 1000, 0 },
+    { "ram", 1000, 0 }
+};
 
 // From https://stackoverflow.com/questions/23143693/retrieving-cpu-load-percent-total-in-windows-with-c
 static float CalculateCPULoad(unsigned long long idleTicks, unsigned long long totalTicks)
@@ -38,6 +54,35 @@ float GetCPULoad()
     return GetSystemTimes(&idleTime, &kernelTime, &userTime) ? CalculateCPULoad(FileTimeToInt64(idleTime), FileTimeToInt64(kernelTime) + FileTimeToInt64(userTime)) : -1.0f;
 }
 
+int getCPUData(char* buf, int bufsz, int* treatDataType)
+{
+    double cpu = GetCPULoad() * 100.0;
+
+    snprintf(buf, bufsz, "%d", (int) cpu);
+
+    return true;
+}
+
+int getRAMData(char* buf, int bufsz, int* treatDataType)
+{
+    // https://stackoverflow.com/questions/63166/how-to-determine-cpu-and-memory-consumption-from-inside-a-process
+    // collect ram
+    MEMORYSTATUSEX memInfo;
+    memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+    GlobalMemoryStatusEx(&memInfo);
+    DWORDLONG totalPhysMem = memInfo.ullTotalPhys;
+    DWORDLONG physMemUsed = memInfo.ullTotalPhys - memInfo.ullAvailPhys;
+
+    snprintf(buf, bufsz, "{ \"total\": %lld, \"used\": %lld }", totalPhysMem, physMemUsed);
+
+    if (nullptr != treatDataType)
+    {
+        *treatDataType = QUASAR_TREAT_AS_JSON;
+    }
+
+    return true;
+}
+
 void quasar_plugin_free(void* ptr, int size)
 {
     if (size > 1)
@@ -50,70 +95,84 @@ void quasar_plugin_free(void* ptr, int size)
     }
 }
 
-int quasar_plugin_init(QuasarPluginInfo* info)
+int quasar_plugin_init(int cmd, QuasarPluginInfo* info)
 {
-    if (!info->logFunc)
+    switch (cmd)
     {
-        return false;
+        case QUASAR_INIT_START:
+        {
+            if (nullptr == info)
+            {
+                return false;
+            }
+
+            if (!info->logFunc)
+            {
+                return false;
+            }
+
+            logFunc = info->logFunc;
+            strcpy_s(info->pluginName, pluginName);
+            strcpy_s(info->pluginCode, pluginCode);
+            strcpy_s(info->author, "me");
+            strcpy_s(info->version, "v1");
+            strcpy_s(info->description, "Sample plugin that queries basic performance numbers");
+
+            // setup data sources
+
+            info->numDataSources = _countof(sources);
+            info->dataSources = sources;
+
+            return true;
+        }
+        case QUASAR_INIT_RESP:
+        {
+            // Process uid returns.
+            // This should also deallocate anything info->dataSources if
+            // it was allocated dynamically
+
+            if (sources[PERF_SRC_CPU].uid != 0)
+            {
+                calltable[sources[PERF_SRC_CPU].uid] = getCPUData;
+            }
+
+            if (sources[PERF_SRC_RAM].uid != 0)
+            {
+                calltable[sources[PERF_SRC_RAM].uid] = getRAMData;
+            }
+
+            return true;
+        }
+        case QUASAR_INIT_SHUTDOWN:
+        {
+            // does nothing in this example, but should deallocate and shutdown any allocated resources
+            return true;
+        }
+        default:
+        {
+            if (logFunc)
+            {
+                logFunc(QUASAR_LOG_WARNING, "Unidentified command");
+            }
+            break;
+        }
     }
 
-    logFunc = info->logFunc;
-    strcpy_s(info->pluginName, pluginName);
-    strcpy_s(info->pluginCode, pluginCode);
-    strcpy_s(info->author, "me");
-    strcpy_s(info->description, "Sample plugin that queries basic performance numbers");
-
-    // setup data sources
-
-    info->numDataSources = 2;
-
-    QuasarPluginDataSource *sources = new QuasarPluginDataSource[2];
-
-    strcpy_s(sources[0].dataSrc, cpuSource);
-    sources[0].refreshMsec = 5000;
-
-    strcpy_s(sources[1].dataSrc, ramSource);
-    sources[1].refreshMsec = 10000;
-
-    info->dataSources = sources;
-
-    return true;
+    return false;
 }
 
-int quasar_plugin_get_data(const char* dataSrc, char* buf, int bufsz, int* convert_data_to_json)
+int quasar_plugin_get_data(unsigned int srcUid, char* buf, int bufsz, int* treatDataType)
 {
-    if (0 == strcmp(dataSrc, cpuSource))
+    if (calltable.count(srcUid) == 0)
     {
-        // collect cpu
-        double cpu = GetCPULoad() * 100.0;
-
-        snprintf(buf, bufsz, "%d", (int)cpu);
-    }
-    else if (0 == strcmp(dataSrc, ramSource))
-    {
-        // https://stackoverflow.com/questions/63166/how-to-determine-cpu-and-memory-consumption-from-inside-a-process
-        // collect ram
-        MEMORYSTATUSEX memInfo;
-        memInfo.dwLength = sizeof(MEMORYSTATUSEX);
-        GlobalMemoryStatusEx(&memInfo);
-        DWORDLONG totalPhysMem = memInfo.ullTotalPhys;
-        DWORDLONG physMemUsed = memInfo.ullTotalPhys - memInfo.ullAvailPhys;
-
-        snprintf(buf, bufsz, "{ \"total\": %lld, \"used\": %lld }", totalPhysMem, physMemUsed);
-
-        if (nullptr != convert_data_to_json)
-        {
-            *convert_data_to_json = true;
-        }
+        char msg[256];
+        snprintf(msg, sizeof(msg), "Plugin '%s' - Unknown source %u", pluginCode, srcUid);
+        logFunc(QUASAR_LOG_WARNING, msg);
     }
     else
     {
-        char msg[256];
-        snprintf(msg, sizeof(msg), "Plugin '%s' - Unknown source %s", pluginCode, dataSrc);
-        logFunc(QUASAR_LOG_WARNING, msg);
-
-        return false;
+        return calltable[srcUid](buf, bufsz, treatDataType);
     }
 
-    return true;
+    return false;
 }
