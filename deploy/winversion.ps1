84957d85d3c007f97e5c5e061b47fde02ab07cdd
Simon <homer12345@gmail.com>
2017-06-02 13:51:02 -0700

    Move version.h into GeneratedFiles/

--- deploy/winversion.ps1
+++ deploy/winversion.ps1
@@ -1,7 +1,15 @@
+# This would be performed during cmake on other platforms
+Write-Host "Generating version.h..."
 $gitdesc = (git describe --tags --always)
 
 $versionfile = Get-Item .\src\version.h.in
+$outpath = ".\GeneratedFiles"
+
+if (!(Test-Path $outpath))
+{
+    New-Item -ItemType Directory -Force -Path $outpath | Out-Null
+}
 
 (Get-Content $versionfile.PSPath) |
     Foreach-Object { $_ -replace "@QUASAR_GIT_VER@", $gitdesc } |
-    Set-Content $versionfile.Basename
+    Set-Content "$($outpath)\$($versionfile.Basename)" -Force
