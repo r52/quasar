a0dd83b9635e54a872acc7decb09dc6a80366152
Simon <homer12345@gmail.com>
2017-07-14 19:05:21 -0700

    Restore Qt::WindowDoesNotAcceptFocus
    Can't find a better way to ignore focus...

--- src/webwidget.cpp
+++ src/webwidget.cpp
@@ -41,7 +41,7 @@ WebWidget::WebWidget(QString widgetName, const QJsonObject& dat, QWidget* parent
     data = dat;
 
     // No frame/border, no taskbar button
-    Qt::WindowFlags flags = Qt::FramelessWindowHint | Qt::SubWindow;
+    Qt::WindowFlags flags = Qt::FramelessWindowHint | Qt::SubWindow | Qt::WindowDoesNotAcceptFocus;
 
     webview = new QuasarWebView(this);
 
