dff71a3da04012987c2d04204688a671d8108446
Simon <homer12345@gmail.com>
2023-01-19 12:56:57 -0800

    Frame time adjusted timer loops

--- quasar/common/timer.h
+++ quasar/common/timer.h
@@ -20,16 +20,25 @@ public:
         SPDLOG_DEBUG("New timer thread with {}ms internal", intv);
         thread = std::jthread{[=, this](std::stop_token token) {
             std::unique_lock<std::mutex> lk(mtx);
+
+            const auto                   origInterval = std::chrono::nanoseconds(std::chrono::milliseconds(intv));
+            auto                         nextSleep    = origInterval;
+
             while (true)
             {
-                if (cv.wait_for(lk, std::chrono::milliseconds(intv), [&] {
+                if (cv.wait_for(lk, nextSleep, [&] {
                         return token.stop_requested();
                     }))
                 {
                     return;
                 }
 
+                auto frameTimeBefore = std::chrono::steady_clock::now();
+
                 fn();
+
+                auto frameTimeAfter = std::chrono::steady_clock::now();
+                nextSleep           = origInterval - (frameTimeAfter - frameTimeBefore);
             }
         }};
 
